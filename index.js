975a2da6d97b9c89d56242e9a5d4df5b5796288b
acofer <adam_cofer@yahoo.com>
2017-07-27 17:08:40 -0400

    Allow for multiple rolling rate limit windows

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 var Hoek = require('hoek');
 var Boom = require('boom');
+var async = require('async');
 var hapiLimiter = 'hapi-limiter';
 
 var internals = {
   defaults: {
     cache: {
-      expiresIn: 1000 * 60 * 15,
+      // https://github.com/hapijs/catbox?ts=2#policy
+      expiresIn: 1000 * 60 * 60 * 24,
       segment: hapiLimiter
     },
-    limit: 15,
-    ttl: 1000 * 60 * 15,
-    generateKeyFunc: function(request) {
-      var methodAndPath = request.method + ':' + request.path + ':';
-      var ip = request.headers['x-forwarded-for'];
+    // Modified from https://www.npmjs.com/package/hapi-limiter#configuration
+    // to allow for multiple limit windows.
+    // Route headers will contain values based on `name: "default"`.
+    limits: [{
+      name: "default",
+      limit: 15,
+      ttl: 1000 * 60 * 15,
+    },{
+      name: "daily",
+      limit: 720,
+      ttl: 1000 * 60 * 60 * 24,
+    }],
 
-      if ( !ip ) {
-        ip = request.info.remoteAddress;
-      }
+    generateKeyFunc: function(request, name) {
+      var key = [];
+      key.push(name);
+      key.push(request.method);
+      key.push(request.path);
+      var ip = request.headers['x-forwarded-for'] || request.info.remoteAddress;
+      key.push(ip);
 
-      return methodAndPath + ip;
+      return key.join(":");
     }
   }
 };
@@ -44,51 +57,54 @@ exports.register =  function(server, options, done) {
     }
 
     var pluginSettings = Hoek.applyToDefaults(globalSettings, routePlugins[hapiLimiter]);
+    request.plugins[hapiLimiter] = {};
 
-    var keyValue = pluginSettings.generateKeyFunc(request);
+    // using async, do parallel check of all limits. If any fails, reply(err).
+    async.each(pluginSettings.limits, function(l, callback){
+      let limit = l.limit,
+        ttl = l.ttl,
+        name = l.name,
+        remaining,
+        reset,
+        keyValue = pluginSettings.generateKeyFunc(request, name);
 
-    cacheClient.get(keyValue, function(err, value, cached) {
-      if ( err ) {
-        return reply(err);
+      if (name === "default"){
+        request.plugins[hapiLimiter].limit = limit;
       }
-      request.plugins[hapiLimiter] = {};
-      request.plugins[hapiLimiter].limit = pluginSettings.limit;
+      cacheClient.get(keyValue, function(err, value, cached) {
+        if ( err ) { return callback(err); }
 
-      if ( !cached ) {
-        var reset = Date.now() + pluginSettings.ttl;
-        return cacheClient.set(keyValue, { remaining: pluginSettings.limit - 1 }, pluginSettings.ttl, function(err) {
-          if ( err ) {
-            return reply(err);
-          }
-          request.plugins[hapiLimiter].remaining = pluginSettings.limit - 1;
+        if ( !cached ) {
+          return cacheClient.set(keyValue, { remaining: limit - 1 }, ttl, function(err) {
+            if ( err ) { return callback(err); }
+            remaining = limit - 1;
+            if (name === "default"){
+              request.plugins[hapiLimiter].remaining = remaining;
+            }
+            callback();
+          });
+        }
+        reset = Date.now() + cached.ttl;
+        remaining = value.remaining - 1;
+        if (name === "default"){
           request.plugins[hapiLimiter].reset = reset;
-          reply.continue();
-        });
-      }
-
-      request.plugins[hapiLimiter].remaining = value.remaining - 1;
-      request.plugins[hapiLimiter].reset = Date.now() + cached.ttl;
-
-      var error;
-      if (  request.plugins[hapiLimiter].remaining < 0 ) {
-        error = Boom.tooManyRequests('Rate Limit Exceeded');
-        error.output.headers['X-Rate-Limit-Limit'] = request.plugins[hapiLimiter].limit;
-        error.output.headers['X-Rate-Limit-Reset'] = request.plugins[hapiLimiter].reset;
-        error.output.headers['X-Rate-Limit-Remaining'] = 0;
-        error.reformat();
-        return reply(error);
-      }
-
-      cacheClient.set(
-        keyValue,
-        { remaining: request.plugins[hapiLimiter].remaining },
-        cached.ttl, function(err) {
-        if ( err ) {
-          return reply(err);
+          request.plugins[hapiLimiter].remaining = remaining;
         }
 
-        reply.continue();
+        if ( remaining < 0 ) {
+          let error = Boom.tooManyRequests('Rate Limit Exceeded');
+          error.output.headers['X-Rate-Limit-Limit'] = limit;
+          error.output.headers['X-Rate-Limit-Reset'] = reset;
+          error.output.headers['X-Rate-Limit-Remaining'] = 0;
+          error.reformat();
+          return callback(error);
+        }
+
+        cacheClient.set(keyValue, { remaining: remaining }, cached.ttl, callback);
       });
+    }, function(err, results){
+      if ( err ){ return reply(err); }
+      reply.continue();
     });
   });
 
@@ -100,19 +116,10 @@ exports.register =  function(server, options, done) {
       pluginSettings[hapiLimiter] &&
       pluginSettings[hapiLimiter].enable
     ) {
-        
-        response = request.response;
-        if(response.isBoom){
-              response.output.headers['X-Rate-Limit-Limit'] = request.plugins[hapiLimiter].limit;
-              response.output.headers['X-Rate-Limit-Remaining'] = request.plugins[hapiLimiter].remaining;
-              response.output.headers['X-Rate-Limit-Reset'] = request.plugins[hapiLimiter].reset;
-        }
-        else{
-            response.headers['X-Rate-Limit-Limit'] = request.plugins[hapiLimiter].limit;
-            response.headers['X-Rate-Limit-Remaining'] = request.plugins[hapiLimiter].remaining;
-            response.headers['X-Rate-Limit-Reset'] = request.plugins[hapiLimiter].reset;
-        }
-      
+      response = request.response.isBoom ? request.response.output : request.response;
+      response.headers['X-Rate-Limit-Limit'] = request.plugins[hapiLimiter].limit;
+      response.headers['X-Rate-Limit-Remaining'] = request.plugins[hapiLimiter].remaining;
+      response.headers['X-Rate-Limit-Reset'] = request.plugins[hapiLimiter].reset;
     }
 
     reply.continue();
